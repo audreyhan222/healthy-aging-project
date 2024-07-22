710cd441538b0c56ec59797b289960bf43e5a1e1
Laurie Dosan <lauriedosan@gmail.com>
2024-07-22 14:15:51 -0700

    yas

--- code/decisionTree.R
+++ code/decisionTree.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(rsample)
-library(glmnet)
-library(rpart)
+library(randomForest)
+
 set.seed(3)
 npha_data <- read_csv("data/NPHA-doctor-visits.csv")
 
@@ -15,6 +15,16 @@ npha_data <- npha_data %>%
   mutate(`Prescription Sleep Medication` = case_when(`Prescription Sleep Medication` == 2 ~1, .default = `Prescription Sleep Medication`)) %>% 
   mutate(Race = case_when(Race != 1 ~ 0, .default = Race)) %>% 
   mutate_all(as.factor) %>%
+  rename(`Number of Doctors Visited` = num_doctors, )
+  
   select(-Age)
 
-summary(npha_data)
+
+data_split <- initial_split(npha_data, prop = 0.7)
+
+train <- training(data_split)
+test <- testing(data_split)
+
+rf <- randomForest(`Number of Doctors Visited` ~ ., data = npha_data, importance = TRUE, proximit = TRUE)
+print(rf)
+
