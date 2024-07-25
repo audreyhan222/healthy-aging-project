49488bb08b611b576d0f92d7691bf018bc3bdfec
Audrey Han <audreyhan222@gmail.com>
2024-07-25 14:22:58 -0700

    added colors to plots

--- code/decision_tree_model.R
+++ code/decision_tree_model.R
@@ -6,9 +6,14 @@ white = npha_data %>%
   filter(Race == 1)
 nonwhite = npha_data %>% 
   filter(Race != 1)
-tree_model <- rpart(`Number of Doctors Visited` ~ ., data = white, 
+tree_model_white <- rpart(`Number of Doctors Visited` ~ ., data = white, 
                     method = "class",control = rpart.control(minsplit = 80, cp=0.005))
 
-rpart.plot(tree_model, box.palette = "gray", main = "Decision Tree")
+rpart.plot(tree_model_white, box.palette = "gray", main = "Decision Tree for Group 1",
+           clip.facs = TRUE)
 
-glimpse(npha_data)
+tree_model_nonwhite <- rpart(`Number of Doctors Visited` ~ ., data = nonwhite, 
+                          method = "class",control = rpart.control(minsplit = 30, cp=0.001))
+
+rpart.plot(tree_model_nonwhite, box.palette = "gray", main = "Decision Tree for Group 2")
+
