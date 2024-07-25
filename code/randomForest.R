5ea45c819f5a557f5addfa5d8af1d396e36afb6c
Laurie Dosan <lauriedosan@gmail.com>
2024-07-25 12:05:32 -0700

    fdsfdsaf

--- code/randomForest.R
+++ code/randomForest.R
@@ -8,57 +8,74 @@ npha_data <- read_csv("data/NPHA-doctor-visits.csv")
 
 #clean data
 npha_data <- npha_data %>%
-  filter(
-    `Phyiscal Health` != -1,
-    `Mental Health` != -1,
-    `Dental Health` != -1,
-    `Trouble Sleeping` != -1,
-    `Prescription Sleep Medication` != -1
-  ) %>%
-  mutate(
-    `Mental Health` = case_when(`Mental Health` == 5 ~ 3, `Mental Health` ==
-                                  4 ~ 3, .default = `Mental Health`)
-  ) %>%
-  mutate(Employment = case_when(Employment == 2 ~ 1, .default = Employment)) %>%
-  mutate(`Trouble Sleeping` = case_when(`Trouble Sleeping` == 2 ~ 1, .default = `Trouble Sleeping`)) %>%
-  mutate(
-    `Prescription Sleep Medication` = case_when(`Prescription Sleep Medication` == 2 ~
-                                                  1, .default = `Prescription Sleep Medication`)
-  ) %>%
-  mutate(Race = case_when(Race != 1 ~ 0, .default = Race)) %>%
-  mutate_all(as.factor) %>%
-  select(-Age)
+  mutate(`Phyiscal Health` = case_when(`Phyiscal Health` == 2 ~ 1,
+                                       `Phyiscal Health` == 5 ~ 4,
+                                       .default = `Phyiscal Health`),
+         `Dental Health` = case_when(`Dental Health` == 2 ~ 1,
+                                     `Dental Health` == 5 ~ 4,
+                                     `Dental Health` == 6 ~ 4,
+                                     .default = `Dental Health`))
 
-npha_data <- npha_data %>%
-  mutate(
-    Physical_Health = `Phyiscal Health`,
-    Mental_Health = `Mental Health`,
-    Dental_Health = `Dental Health`,
-    Employment = Employment,
-    Trouble_Sleeping = `Trouble Sleeping`,
-    Stress_Sleeping = `Stress Keeps Patient from Sleeping`,
-    Pain_Sleeping = `Pain Keeps Patient from Sleeping`,
-    Bathroom_Sleeping = `Bathroom Needs Keeps Patient from Sleeping`,
-    Unknown_Sleeping = `Uknown Keeps Patient from Sleeping`,
-    Medication_Sleeping = `Medication Keeps Patient from Sleeping`,
-    Prescription_Sleep_Medication = `Prescription Sleep Medication`,
-    Race = Race,
-    .keep = "unused"
-  )
+npha_data <- npha_data %>% 
+  filter(`Phyiscal Health` != -1, `Mental Health` != -1, `Dental Health` != -1, `Trouble Sleeping` != -1, `Prescription Sleep Medication` != -1) %>%
+  mutate(`Mental Health` = case_when(`Mental Health` == 5 ~3, `Mental Health` ==4 ~3, .default = `Mental Health`)) %>% 
+  mutate(Employment = case_when(Employment == 2 ~1, .default = Employment)) %>% 
+  mutate(`Trouble Sleeping` = case_when(`Trouble Sleeping` == 2 ~1, .default = `Trouble Sleeping`)) %>% 
+  mutate(`Prescription Sleep Medication` = case_when(`Prescription Sleep Medication` == 2 ~1, .default = `Prescription Sleep Medication`)) %>% 
+  mutate(Race = case_when(Race != 1 ~ 0, .default = Race)) %>% 
+  dplyr::select(-Age)
+
+npha_data <- npha_data %>% 
+  mutate(Physical_Health = `Phyiscal Health`,
+         Mental_Health = `Mental Health`,
+         Dental_Health = `Dental Health`,
+         Employment = Employment,
+         Trouble_Sleeping = `Trouble Sleeping`,
+         Stress_Sleeping = `Stress Keeps Patient from Sleeping`,
+         Pain_Sleeping = `Pain Keeps Patient from Sleeping`,
+         Bathroom_Sleeping = `Bathroom Needs Keeps Patient from Sleeping`,
+         Unknown_Sleeping = `Uknown Keeps Patient from Sleeping`,
+         Medication_Sleeping = `Medication Keeps Patient from Sleeping`,
+         Prescription_Sleep_Medication = `Prescription Sleep Medication`,
+         Race = Race,
+         .keep = "unused") %>% 
+  mutate_all(as.factor)
 
 
-data_split <- initial_split(npha_data, prop = 0.7)
 
-train <- training(data_split)
-test <- testing(data_split)
+white <- npha_data %>% 
+  filter(Race == 1) %>% 
+  dplyr::select(-Race)
 
-rf <- randomForest(
+
+nonwhite <- npha_data %>% 
+  filter(Race != 1) %>% 
+  dplyr::select(-Race)
+
+
+rf_white <- randomForest(
   `Number of Doctors Visited` ~ .,
-  data = train,
+  data = white,
   importance = TRUE,
   proximity = TRUE,
   ntrees = 500
 )
-preds <- predict(rf, newdata = test[-1])
 
-mean(preds == test$`Number of Doctors Visited`)
+
+# make dataframe from importance() output
+imp_df_white <- importance(rf_white) %>% 
+  data.frame() %>% 
+  mutate(feature = row.names(.)) 
+
+# plot dataframe
+ggplot(imp_df_white, aes(x = reorder(feature, MeanDecreaseGini), 
+                        y = MeanDecreaseGini), fill = y) +
+  geom_bar(stat='identity') +
+  coord_flip() +
+  theme_classic() +
+  labs(
+    x     = "Feature",
+    y     = "Importance",
+    title = "Feature Importance: White Subset"
+  ) +
+  scale_fill_gradient(low = "steelblue", high ="slateblue")
